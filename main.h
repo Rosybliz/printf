96bab575a9f50cabe5d6f4eb8ec179462e14073c
Rosybliz <bchoco961@gmail.com>
2023-08-21 12:14:18 -0700

    Header and used updated

--- main.h
+++ main.h
@@ -10,19 +10,37 @@
 #define  SUB 1
 #define ZERO 4
 #define ADD 2
-
+#define BIGSIZE 2
+#define SMALLSIZE 1
 /********* WRITES*************/
 
 int write_char(int c, char my_buff[], int flg,
-		int prcs, int size, int wid)
+		int prcs, int size, int wid);
 int write_ptr(int pnd, char my_buff[], int flg,
-		int len, char xtraC, char bar, int bar_start, int wid)
+		int len, char xtraC, char bar, int bar_start, int wid);
 int write_unsigned(int is_neg, int pnd, char my_buff, int flg,
-		int prcs, int wid, int size)
+		int prcs, int wid, int size);
 int write_numb(int pnd, char my_buff[], int flg,
-		int prcs, int len, char xtraC, char bar, int wid)
+		int prcs, int len, char xtraC, char bar, int wid);
 int write_no(int is_neg, int pnd, char my_buff[], int flg,
-		int prcs, int size, int wid)
+		int prcs, int size, int wid);
+/*************USED************/
+int my_digit(char A);
+long int size_numb(long int numb, int size);
+int put_hexa(char ascii, char my_buff, int x);
+long int size_unsigned(unsigned long int numb,  int size);
+int is_print(char c);
+
+/**************PRINTALL***********/
+
+int print_unprintable(va_list my_specs, char my_buff,
+		int flg, int prcs, int wid, int size);
+int print_reverse(va_list my_specs, char my_buff, int flg,
+		int prcs, int wid, int size);
+int print_rotstring(va_list my_specs, char my_buff, int size,
+		int prcs, int wid, int flg);
+int print_ptr(va_list my_specs, char my_buff,
+		int size, int wid, int prcs, int flg);
 
 int _printf(const char *format, ...);
 
--- used.c
+++ used.c
@@ -10,9 +10,9 @@
 
 long int size_numb(long int numb, int size)
 {
-	if (size == LONGS)
+	if (size == BIGSIZE)
 		return (numb);
-	else if (size == SHORTS)
+	else if (size == SMALLSIZE)
 		return ((short)numb);
 	return ((int)numb);
 }
@@ -25,4 +25,51 @@ int my_digit(char A)
 {
 	if (A <= '9' && A >= '0')
 		return (0);
+}
+/**
+ * size_unsigned- a function that casts a number to a size
+ * @numb: the number
+ * @size: the size
+ * Return: the new size
+ */
+
+long int size_unsigned(unsigned long int numb,  int size)
+{
+	if (size == SMALLSIZE)
+		return ((unsigned short)numb);
+	else if (size == BIGSIZE)
+		return (numb);
+	return ((unsigned int)num);
+}
+
+/**
+ * put_hexa- a funtion that appends ascii in hexadecimal form
+ * @ascii: ASCII code
+ * @my_buff: the character array
+ * @x: index
+ * Return: 3
+ */
+int put_hexa(char ascii, char my_buff, int x)
+{
+	char map[] = "0123456789ABCDEF";
+
+	if (ascii < 0)
+		ascii *= -1;
+	my_buff[x++] = '\\';
+	my_buff[x++] = 'x';
+	my_buff[x++] = map[ascii / 16];
+	my_buff[x] = map[ascii % 16];
+
+	return (3);
+}
+/**
+ * is_print- a function that checks if a character is printable
+ * @c: the character
+ * Return: return 1
+ */
+int is_print(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+	return (0);
 }
